1c6ff9c1cf848ad5af5f1e830362cc9f3438b1fc
seanmcpe <seanmcpex@gmail.com>
2015-05-28 07:44:22 -0400

    Update Main.php

--- src/Sean_M/Main.php
+++ src/Sean_M/Main.php
@@ -1,1 +1,12 @@
+<?php
+namespace Sean_M\DeathPopup;
 
+use pocketmine\Player;
+use pocketmine\event\player\PlayerDeathEvent;
+
+class Main extends PluginBase implements Listener{
+  
+  public function onEnable(){      
+    $this->getServer()->getPluginManager()->registerEvents($this);
+    $this->getLogger()->info("DeathPopup enabled!");
+  }
