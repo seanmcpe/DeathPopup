d42ccd62e51c57b25523a092595ca8805c378ccf
seanmcpe <seanmcpex@gmail.com>
2015-05-28 07:46:42 -0400

    Update Main.php

--- src/Sean_M/Main.php
+++ src/Sean_M/Main.php
@@ -10,3 +10,15 @@ class Main extends PluginBase implements Listener{
     $this->getServer()->getPluginManager()->registerEvents($this);
     $this->getLogger()->info("DeathPopup enabled!");
   }
+
+  $p = $event->getPlayer();
+  $causeId = $p->getLastDamageCause()->getCause();
+  switch($causeId){
+    case EntityDamageEvent::CAUSE_DROWNING:
+      $text = "You drowned!";
+      break;
+    case EntityDamageEvent::CAUSE_FALL:
+      $text = "You fell from a high place!";
+      break;
+  }
+  if(isset($text)) $p->sendPopup($text);
