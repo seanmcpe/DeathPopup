391e2cdd8ae3909bc3eafb19e65d11de04103580
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-05-28 23:10:16 +0800

    Added a few more events

--- src/Sean_M/Main.php
+++ src/Sean_M/Main.php
@@ -3,6 +3,8 @@ namespace Sean_M\DeathPopup;
 
 use pocketmine\Player;
 use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\EntityDamageByBlockEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
 
 class Main extends PluginBase implements Listener{
   
@@ -20,5 +22,36 @@ class Main extends PluginBase implements Listener{
     case EntityDamageEvent::CAUSE_FALL:
       $text = "You fell from a high place!";
       break;
+    case EntityDamageEvent::CAUSE_LAVA:
+      $text = "You tried to swim in lava!";
+      break;
+    case EntityDamageEvent::CAUSE_FIRE:
+      $text = "You burned to death!";
+      break;
+    case EntityDamageEvent::CAUSE_FIRE_TICK:
+      $text = "You burned to death!";
+      break;
+    case EntityDamageEvent::CAUSE_SUICIDE:
+      $text = "You died!"
+      break;
+    case EntityDamageEvent::CAUSE_CONTACT:
+				if($cause instanceof EntityDamageByBlockEvent){
+					if($cause->getDamager()->getId() === Block::CACTUS){
+						$text = "You got pricked to death!";
+					}
+				}
+				break;
+		case EntityDamageEvent::CAUSE_PROJECTILE:
+				if($cause instanceof EntityDamageByEntityEvent){
+					$e = $cause->getDamager();
+					if($e instanceof Living){
+						$text = "You were shot by $params[].";
+						$params[] = $e->getName();
+						break;
+					}else{
+						$params[] = "Unknown";
+					}
+				}
+				break;		
   }
   if(isset($text)) $p->sendPopup($text);
